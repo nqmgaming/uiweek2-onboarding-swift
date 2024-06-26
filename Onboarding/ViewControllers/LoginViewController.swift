72b5ceb9bbda11ee7202dc2f26ab056f3a26c761
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-06-26 07:53:18 +0700

    declare variable for login view controller

--- Onboarding/ViewControllers/LoginViewController.swift
+++ Onboarding/ViewControllers/LoginViewController.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 class LoginViewController : ViewController {
+
+    let backgroundImageView = UIImageView()
+    let logoImageView = UIImageView()
+    let loginLabel = UILabel()
+    let emailTextField = UITextField()
+    let passwordTextField = UITextField()
+    let forgotPasswordLabel = UILabel()
+    let loginButton = UIButton()
+    let lineLeadingView = UIView()
+    let lineTrailingView = UIView()
+    let orLabel = UILabel()
+    let googleButton = UIButton()
+    let facebookButton = UIButton()
+    let appleButton = UIButton()
+
     override func viewDidLoad() {
         view.backgroundColor = .cyan
     }
